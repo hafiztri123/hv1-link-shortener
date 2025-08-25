8b666dc1300d3b117f119ee6038e1e938062988a
Hafizh Tri <programmer-fe2@qwertysystem.net>
2025-08-25 17:35:01 +0700

    feat: wip registering route for health check

---
+++ internal/redis/redis.go
@@ -0,0 +1,26 @@
+package redis
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func NewClient(ctx context.Context) (*redis.Client, error) {
+	rdbAddr, ok := os.LookupEnv("REDIS_URL")
+	if !ok {
+		log.Fatal("REDIS_URL Environment variable  not set")
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: rdbAddr,
+	})
+
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return rdb, nil
+}

bfdc033802046cd42060a988471842a48e8b6f1f
Hafizh Tri <programmer-fe2@qwertysystem.net>
2025-09-04 14:53:09 +0700

    test: creating test for custom errors

---
+++ internal/user/user_errors_test.go
@@ -0,0 +1,44 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestErrors(t *testing.T) {
+	testCases := []struct{
+		name string
+		err error
+		wantErrBody string
+	}{
+		{
+			name: "Email already exists",
+			err: EmailAlreadyExists,
+			wantErrBody: "Email already exists",
+		},
+		{
+			name: "User not found",
+			err: UserNotFound,
+			wantErrBody: "User not found",
+		},
+		{
+			name: "Invalid credentials",
+			err: InvalidCredentials,
+			wantErrBody: "Invalid credentials",
+		},
+		{
+			name: "Unexpected error",
+			err: UnexpectedError,
+			wantErrBody: "Unexpected error has occured",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.err
+			assert.Equal(t, tc.wantErrBody, err.Error())
+		})
+	}
+}

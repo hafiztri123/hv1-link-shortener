42ee416324bc7fd7d3e0574917b6fd6eae51fcc2
astra <hafiz.triwahyu@gmail.com>
2025-09-29 12:04:31 +0700

    fix: fix routing

--- services/app/internal/api/router.go
+++ services/app/internal/api/router.go
@@ -34,43 +34,40 @@ func NewServer(db DB, redis *redis.Client, urlService url.URLService, userServic
 		urlService:   urlService,
 		userService:  userService,
 		tokenService: ts,
-		geoDb:        geoDb,
-	}
+		geoDb:        geoDb}
 }
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 
-	// r.Use(RateLimiter(10, 20))
 	r.Use(RedisRateLimiter(s.redis, 20, 1*time.Minute))
-
 	r.Use(metrics.PrometheusMiddleware)
+
 	r.Route("/api/v1", func(v1 chi.Router) {
 		v1.Get("/health", s.healthCheckHandler)
-
 		v1.Post("/user/register", s.handleRegister)
 		v1.Post("/user/login", s.handleLogin)
 		v1.Handle("/metrics", promhttp.Handler())
 
 		v1.Route("/url", func(url chi.Router) {
 			url.Use(MetadataMiddleware(s.geoDb))
-			url.Get("/url/{shortCode}", s.handleFetchURL)
-			url.Get("/url/{shortCode}/qr", s.handleGenerateQR)
 
+			url.Get("/{shortCode}", s.handleFetchURL)
+			url.Get("/{shortCode}/qr", s.handleGenerateQR)
+
+			url.Group(func(protected chi.Router) {
+				protected.Use(AuthMiddleware(s.tokenService, true))
+				protected.Post("/shorten", s.handleCreateURL)
+				protected.Post("/shorten/bulk", s.handleCreateURL_Bulk)
+			})
 		})
 
-		v1.Route("/url", func(protected chi.Router) {
-			protected.Use(AuthMiddleware(s.tokenService, true))
-			protected.Post("/shorten", s.handleCreateURL)
-			protected.Post("/shorten/bulk", s.handleCreateURL_Bulk)
-		})
-
-		v1.Route("/user", func(protected chi.Router) {
-			protected.Use(AuthMiddleware(s.tokenService, false))
-			protected.Get("/history", s.handleFetchUserURLHistory)
+		// User routes
+		v1.Route("/user", func(user chi.Router) {
+			user.Use(AuthMiddleware(s.tokenService, false))
+			user.Get("/history", s.handleFetchUserURLHistory)
 		})
 	})
 
 	return r
-
 }

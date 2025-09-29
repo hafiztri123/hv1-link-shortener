62f0d2857d3010fc2d5ed535c6b80f06409879c9
astra <hafiz.triwahyu@gmail.com>
2025-09-29 17:01:12 +0700

    feat: add consumer

--- services/worker/internal/queue/consumer.go
+++ services/worker/internal/queue/consumer.go
@@ -2,8 +2,12 @@ package queue
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"hafiztri123/worker-link-shortener/internal/queue/metadata"
+	"hpj/hv1-link-shortener/shared/models"
 	"log/slog"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -16,9 +20,10 @@ type Consumer struct {
     conn    *amqp.Connection
     channel *amqp.Channel
     queueLabel   string
+	metadataRepository *metadata.Repository
 }
 
-func NewConsumer(addr, queueLabel string) (*Consumer, error) {
+func NewConsumer(addr, queueLabel string, metadataRepository *metadata.Repository) (*Consumer, error) {
 	conn, err := amqp.Dial(addr)
 	if err != nil {
 		slog.Error("failed to dial rabbit mq server", "err", err)
@@ -28,8 +33,60 @@ func NewConsumer(addr, queueLabel string) (*Consumer, error) {
 	ch, err := conn.Channel()
 	if err != nil {
 		slog.Error("failed to established channel in rabbit mq connection", "err", err)
+		conn.Close()
 		return nil, err
 	}
+
+	if err := setupDeadLetterQueue(ch, queueLabel); err != nil {
+		slog.Error("failed to create dead letter exchange", "error", err)
+		conn.Close()
+		ch.Close()
+		return nil, err
+	}
+
+	if err := setupRetryQueue(ch, queueLabel); err != nil {
+		slog.Error("failed to create retry queue", "error", err)
+		conn.Close()
+		ch.Close()
+		return nil, err
+	}
+
+	_, err = ch.QueueDeclare(
+		queueLabel,
+		true,
+		false,
+		false,
+		false,
+		amqp.Table{
+			"x-dead-letter-exchange": queueLabel + ".dlx",
+			"x-dead-letter-routing-key": queueLabel + ".dlq",
+		},
+	)
+
+	 if err != nil {
+        ch.Close()
+        conn.Close()
+		slog.Error("failed to queue declare with dead letter mechanism", "error", err)
+        return nil, err
+    }
+
+	err = ch.Qos(1, 0, false)
+
+	if err != nil {
+        ch.Close()
+        conn.Close()
+		slog.Error("failed to set quality of service", "error", err)
+        return nil, err
+    }
+	
+
+
+	return &Consumer{
+		conn: conn,
+		channel: ch,
+		queueLabel: queueLabel,
+		metadataRepository: metadataRepository,
+	}, nil
 
 
 }
@@ -60,13 +117,67 @@ func (c *Consumer) StartConsuming(ctx context.Context, handler func([]byte) erro
 				slog.Error("error in getting value from channel")
 				return fmt.Errorf("channel closed")
 			}
+
+			c.handleMetadataMessage(msg)
 		}
 	}
 }
 
-func (c *Consumer) handleMessage(msg amqp.Delivery, handler func([]byte) error){
+func (c *Consumer) handleMetadataMessage(msg amqp.Delivery) {
 	retryCount := getRetryCount(msg.Headers)
+	var data *models.Click
 
+	err := json.Unmarshal(msg.Body, &data)
+
+	if err != nil {
+		slog.Error("failed to handle data", "err", err)
+		return 
+	}
+
+	contextTimeout, cancel := context.WithTimeout(context.Background(), 5 *time.Second)
+	defer cancel()
+
+	if err := c.metadataRepository.InsertMetadata(contextTimeout, data); err != nil {
+		if retryCount >= MaxRetries {
+			msg.Nack(false, false)
+			return
+		}
+
+		if err := c.sendToRetryQueue(msg, retryCount+1); err != nil {
+			msg.Nack(false, true)
+			return
+		}
+
+		msg.Ack(false)
+		return
+	}
+
+	msg.Ack(false)
+
+}
+
+func (c *Consumer) sendToRetryQueue(msg amqp.Delivery, retryCount int32) error {
+	retryQueueLabel := c.queueLabel + ".retry"
+	retryExchangeLabel := c.queueLabel + ".retry.exchange"
+
+	headers := make(amqp.Table)
+	if msg.Headers != nil {
+		headers = msg.Headers
+	}
+	headers["x-retry-count"] = retryCount
+
+	return c.channel.Publish(
+		retryExchangeLabel,
+		retryQueueLabel,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: msg.ContentType,
+			Body: msg.Body,
+			DeliveryMode: amqp.Persistent,
+			Headers: headers,
+		},
+	)
 }
 
 func (c *Consumer) Close() error {
